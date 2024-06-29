4418111e285b4700583d84d7ea0b8f96286bb7d4
MateuszApps <mateusz.lukowski3@gmail.com>
2024-06-29 20:15:06 +0200

    add styles and configure index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import loadHome from './home';
+import loadMenu from './menu';
+import loadAbout from './about';
+import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHome();
-})
+
+    const homeButton = document.querySelector('button:nth-child(1)');
+    const menuButton = document.querySelector('button:nth-child(2)');
+    const aboutButton = document.querySelector('button:nth-child(3)');
+
+    homeButton.addEventListener('click', loadHome);
+    menuButton.addEventListener('click', loadMenu);
+    aboutButton.addEventListener('click', loadAbout);
+});
+
+console.log('Webpack is working!');
